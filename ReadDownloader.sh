e057808583a1f876b5827397676c5c1af529c85b
Hayden <hayden@DESKTOP-KB84CNE.localdomain>
2026-03-14 00:15:11 +0000

    Copied sh file from practice

---
+++ ReadDownloader.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+#######Specify download FTPs below
+Patient1 = "ERR3163055"
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR316/005/ERR3163055/ERR3163055_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR316/005/ERR3163055/ERR3163055_2.fastq.gz
+
+Patient2 = "ERR3163059"
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR316/009/ERR3163059/ERR3163059_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR316/009/ERR3163059/ERR3163059_2.fastq.gz
